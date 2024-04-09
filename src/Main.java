b7b6a7cb5f71bd8efe8c3308d33dfcafff3c7e08
tuyen9915 <hat453430@gmail.com>
2024-04-09 11:21:11 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    Fan fan1 = new Fan(3,true,10,"yellow");
+        System.out.println(fan1);
+
+    Fan fan2 = new Fan(2,false,5,"blue");
+        System.out.println(fan2);
+    }
+}
